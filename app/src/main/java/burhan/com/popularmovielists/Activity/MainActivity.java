94e140f4b524dbadca0a21d43d9681a090ffb1d3
bshakir <burhanshakir96@gmail.com>
2017-11-18 01:06:05 +0000

    Initial commit

---
+++ app/src/main/java/burhan/com/popularmovielists/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package burhan.com.popularmovielists.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import burhan.com.popularmovielists.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
